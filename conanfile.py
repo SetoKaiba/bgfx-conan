fc257c9339adecab8e339a82f46ba34145b0f71a
Seto Kaiba <seto.kaiba@unity3d.com>
2021-03-31 12:50:32 +0800

    Initial support for Macos

--- conanfile.py
+++ conanfile.py
@@ -44,24 +44,40 @@ class BgfxConan(ConanFile):
         os.rename(glob.glob("bimg-*")[0], self._bimg_subfolder)
 
     def build(self):
+        cmd = "cd bgfx && "
         if self.settings.os == "Windows":
             genie_path = os.path.join(
                 "..", "bx", "tools", "bin", "windows", "genie")
-            cmd = "cd bgfx && "
-            cmd += genie_path
-            if(self.options.with_tools):
-                cmd += " --with-tools"
-            if(self.options.with_combined_examples):
-                cmd += " --with-combined-examples"
-            if(self.options.with_shared_lib):
-                cmd += " --with-shared-lib"
+        if self.settings.os == "Macos":
+            genie_path = os.path.join(
+                "..", "bx", "tools", "bin", "darwin", "genie")
+        cmd += genie_path
+        if self.options.with_tools:
+            cmd += " --with-tools"
+        if self.options.with_combined_examples:
+            cmd += " --with-combined-examples"
+        if self.options.with_shared_lib:
+            cmd += " --with-shared-lib"
+        if self.settings.os == "Windows":
             cmd += " --with-windows=" + str(self.options.with_windows)
             cmd += " vs" + str(self.options.vs_version)
-            self.run(cmd)
+        if self.settings.os == "Macos":
+            cmd += " --gcc=osx-x64"
+            cmd += " gmake"
+        self.run(cmd)
+        if self.settings.os == "Windows":
             msbuild = MSBuild(self)
             sln_path = os.path.join(
                 "bgfx", ".build", "projects", "vs" + str(self.options.vs_version), "bgfx.sln")
             msbuild.build(sln_path, build_type=self.settings.build_type)
+        if self.settings.os == "Macos":
+            project_path = os.path.join(
+                "bgfx", ".build", "projects", "gmake-osx-x64")
+            build_cmd = "make -C "
+            build_cmd += project_path
+            build_cmd += " config=" + str(self.settings.build_type).lower()
+            self.run(build_cmd)
+            
 
     def package(self):
         self.copy("*", dst="include",
@@ -70,16 +86,26 @@ class BgfxConan(ConanFile):
                   src=os.path.join(self.build_folder, self._bx_subfolder, "include"))
         self.copy("*", dst="include",
                   src=os.path.join(self.build_folder, self._bimg_subfolder, "include"))
-        if(self.settings.os == "Windows"):
-            if(self.settings.arch == "x86"):
+        if self.settings.os == "Windows":
+            if self.settings.arch == "x86":
                 platform = "win32_vs" + str(self.options.vs_version)
-            if(self.settings.arch == "x86_64"):
+            if self.settings.arch == "x86_64":
                 platform = "win64_vs" + str(self.options.vs_version)
             self.copy("*.lib", dst="lib",
                       src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
             self.copy("*.exe", dst="bin",
                       src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
             self.copy("*.dll", dst="bin",
+                      src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
+        if self.settings.os == "Macos":
+            platform = "osx-x64"
+            self.copy("*.a", dst="lib",
+                      src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
+            self.copy("*" + str(self.settings.build_type), dst="bin",
+                      src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
+            self.copy("example*" + str(self.settings.build_type), dst="bin",
+                      src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
+            self.copy("*.dylib", dst="bin",
                       src=os.path.join(self.build_folder, self._bgfx_subfolder, ".build", platform, "bin"))
 
     def package_info(self):
